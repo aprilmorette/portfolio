c40c8da42ed7ee7435569d0c15b4947b9418b7dd
aprilmorette <aprilmorette@gmail.com>
2025-07-14 05:50:15 -0500

    added card formatting

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <section id="about" className="bg-background px-4 pb-10 max-w-5xl mx-auto">
+    <section id="about" className="px-4 pt-10 pb-15 max-w-6xl mx-auto">
       <h2 className="text-center mb-5">about me</h2>
 
-      <div className="flex flex-col lg:flex-row items-center justify-around p-6 gap-5 bg-white/80 rounded-xl shadow-md">
+      <div className="card flex flex-col lg:flex-row items-center justify-around p-8 gap-5">
         <Image
           src="/profile-picture.jpg"
           alt="April Duff profile picture"
@@ -17,7 +17,7 @@ const About = () => {
 
         <div className="flex flex-col gap-5">
           <p
-            className="text-base sm:text-lg leading-relaxed sm:leading-loose text-foreground max-w-xl"
+            className="text-base sm:text-lg leading-relaxed sm:leading-loose max-w-xl"
             aria-label="bio"
           >
             I am a recent Computer Science graduate from Oklahoma State
@@ -35,67 +35,33 @@ const About = () => {
 
       {/* skills section */}
       <div>
-        <h3 className="mt-10 mb-5">development</h3>
+        <h3 className="mt-10 mb-5 text-center">skills</h3>
+
+        <h4 className="mt-6 mb-4">development</h4>
 
         <div className="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto">
-          <div className="grid grid-cols-4 justify-items-center items-center lg:grid-cols-5 gap-6 font-body">
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">JavaScript</p>
-            </div>
+          <div id="skills" className="flex flex-wrap justify-items-center items-center gap-6 font-body">
 
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">HTML</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">CSS</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">Java</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">SQL</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">React</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">Tailwind CSS</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">Visual Studio</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">Vercel</p>
-            </div>
+            <div><p className="text-sm sm:text-lg">JavaScript</p></div>
+            <div><p className="text-sm sm:text-lg">HTML</p></div>
+            <div><p className="text-sm sm:text-lg">CSS</p></div>
+            <div><p className="text-sm sm:text-lg">Java</p></div>
+            <div><p className="text-sm sm:text-lg">React</p></div>
+            <div><p className="text-sm sm:text-lg">Tailwind CSS</p></div>
+            <div><p className="text-sm sm:text-lg">SQL</p></div>
+            <div><p className="text-sm sm:text-lg">Visual Studio</p></div>
+            <div><p className="text-sm sm:text-lg">Git</p></div>
+            <div><p className="text-sm sm:text-lg">Vercel</p></div>
           </div>
         </div>
 
-        <h3 className="mt-10 mb-5">design</h3>
-
+        <h4 className="mt-10 mb-4">design</h4>
         <div className="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto">
-          <div className="grid grid-cols-4 justify-items-center items-center lg:grid-cols-5 gap-6 font-body">
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">Figma</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">Milanote</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">Notion</p>
-            </div>
-
-            <div className="justify-center border border-accent rounded-full text-center p-4">
-              <p className="text-sm sm:text-lg">Pen and Paper</p>
-            </div>
+          <div id="skills" className="flex flex-wrap justify-items-center items-center gap-6 font-body">
+            <div><p className="text-sm sm:text-lg">Figma</p></div>
+            <div><p className="text-sm sm:text-lg">Milanote</p></div>
+            <div><p className="text-sm sm:text-lg">Notion</p></div>
+            <div><p className="text-sm sm:text-lg">Pen and Paper</p></div>
           </div>
         </div>
       </div>
--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -4,30 +4,30 @@ const Experience = () => {
   return (
     <section
       id="experience"
-      className="bg-background px-4 py-15 max-w-5xl mx-auto"
+      className="px-4 pt-10 pb-15 max-w-6xl mx-auto"
     >
       <h2 className="text-center mb-5">experience</h2>
 
-      <div className="flex justify-around">
+      <div className="flex justify-around gap-8">
         {/* OSU CEAT IT */}
-        <div className="flex flex-col items-center">
-          <p>MAY 2022 - MAY 2025</p>
+        <div className="card flex flex-col items-center p-8 basis-1/2">
+          <p className="italic">MAY 2022 - MAY 2025</p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="size-8 sm:size-12"
+            className="size-8 sm:size-12"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
             />
           </svg>
           <p>Student IT Technician</p>
-          <p>Oklahoma State University</p>
+          <a href="https://ceat.okstate.edu/itservices/" aria-label="OSU CEAT IT Website">Oklahoma State University CEAT IT</a>
 
           <div className="text-center">
             <p>
@@ -40,25 +40,25 @@ const Experience = () => {
         </div>
 
         {/* QuikTrip */}
-        <div className="flex flex-col items-center">
-          <p>JUNE 2020 - AUGUST 2021</p>
+        <div className="card flex flex-col items-center p-8 basis-1/2">
+          <p className="italic">JUNE 2020 - AUGUST 2021</p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="size-8 sm:size-12"
+            className="size-8 sm:size-12"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
             />
           </svg>
 
           <p>Store Clerk</p>
-          <p>QuikTrip</p>
+          <a href="https://www.quiktrip.com/" aria-label="QuikTrip Website">QuikTrip</a>
           <div className="text-center">
             <p>Managed customer transactions and daily cash handling.</p>
             <p>
